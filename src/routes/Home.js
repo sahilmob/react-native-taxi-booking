430b4bafed642dc6c2ba91642e9246a515123e2e
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-10-16 01:26:43 +0300

    code and styles clean up

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -9,25 +9,18 @@ class Home extends Component {
     drawerLabel: "Home"
   };
   render() {
-    return (<View accessible={true} style={styles.container}>
-
-
-
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-
-
-    </View>
-
-
-
+    return (
+      <View accessible={true} style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
     );
   }
 }
@@ -48,25 +41,10 @@ const mapDispatchToProps = dispatch => {
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
 
 const styles = StyleSheet.create({
-  fadeIn: {
-    width: 250,
-    height: 50,
-    backgroundColor: '#bdc3c7',
-  },
   container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    flex: 1
   },
   map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
+    flex: 1
+  }
 });
