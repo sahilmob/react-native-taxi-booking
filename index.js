470a2cbed793fc587e3d3a32abb56603195dfb40
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-10-15 05:26:28 +0300

    setup redux

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
+import React from "react";
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './src/store/reducer'
 
-AppRegistry.registerComponent(appName, () => App);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+const AppContainer = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => AppContainer);
