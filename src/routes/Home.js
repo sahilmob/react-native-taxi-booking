e963b95006bf1abac92cdd369a48e67d0ef5613d
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-10-16 01:21:03 +0300

    fixed untracked files

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,19 +2,33 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 import { Icon, Button, Container, Header, Content } from 'native-base'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 class Home extends Component {
   static navigationOptions = {
     drawerLabel: "Home"
   };
   render() {
-    return (
-      <Container>
-        <Content>
-          <Text>Home Screen</Text>
-        </Content>
-      </Container>
-    )
+    return (<View accessible={true} style={styles.container}>
+
+
+
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+
+
+    </View>
+
+
+
+    );
   }
 }
 
@@ -34,9 +48,25 @@ const mapDispatchToProps = dispatch => {
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
 
 const styles = StyleSheet.create({
+  fadeIn: {
+    width: 250,
+    height: 50,
+    backgroundColor: '#bdc3c7',
+  },
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-})
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
